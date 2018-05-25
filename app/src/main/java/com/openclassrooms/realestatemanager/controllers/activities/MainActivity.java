85422321e54573b48f93c78e5cb3220c75d5cd66
leoberenger <leoberenger@gmail.com>
2018-05-25 17:27:11 +0200

    Added Toolbar and menu

--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/MainActivity.java
@@ -3,7 +3,10 @@ package com.openclassrooms.realestatemanager.controllers.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,18 +17,62 @@ import com.openclassrooms.realestatemanager.controllers.fragments.MainFragment;
 import com.openclassrooms.realestatemanager.managers.PropertiesMgr;
 import com.openclassrooms.realestatemanager.utils.Utils;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     String PROPERTY_ID = "PROPERTY_ID";
+    @BindView(R.id.toolbar) Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        this.configureToolbar();
 
         //this.configureMapsBtn();
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        //Get to SearchManager or Notification Activity
+        switch (item.getItemId()){
+            case R.id.menu_search:
+                Log.e("MainActivity", "Search Btn clicked");
+                //Intent intent1 = new Intent(this, SearchActivity.class);
+                //startActivity(intent1);
+                return true;
+            case R.id.menu_map:
+                Log.e("MainActivity", "Map Btn clicked");
+                //Intent intent2 = new Intent(this, MapsActivity.class);
+                //startActivity(intent2);
+                return true;
+            case R.id.menu_add:
+                Log.e("MainActivity", "Add new Btn clicked");
+                return true;
+            case R.id.menu_edit:
+                Log.e("MainActivity", "Edit Btn clicked");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void configureToolbar(){
+        setSupportActionBar(mToolbar);
+    }
+
+    
 /*
     private void configureMapsBtn(){
         Button mapsBtn = (Button) findViewById(R.id.activity_main_maps_btn);
--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/SearchActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/SearchActivity.java
@@ -3,6 +3,8 @@ package com.openclassrooms.realestatemanager.controllers.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.openclassrooms.realestatemanager.R;
+
 public class SearchActivity extends AppCompatActivity {
 
     @Override
