f1da597973f5b263ae971aa90ec6fce8be9abc79
leoberenger <leoberenger@gmail.com>
2018-05-28 18:06:09 +0200

    Added AgentDAO

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/dao/AgentDAO.java
@@ -0,0 +1,22 @@
+package com.openclassrooms.realestatemanager.database.dao;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import com.openclassrooms.realestatemanager.models.Agent;
+
+@Dao
+public interface AgentDAO {
+
+    public interface UserDao {
+
+        @Insert(onConflict = OnConflictStrategy.REPLACE)
+        void createAgent(Agent agent);
+
+        @Query("SELECT * FROM Agent WHERE id = :agentId")
+        LiveData<Agent> getAgent(long agentId);
+    }
+}
