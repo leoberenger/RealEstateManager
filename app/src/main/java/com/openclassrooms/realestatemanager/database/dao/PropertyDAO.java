27a2cfe36f32012c8442f83313766f3da247d62f
leoberenger <leoberenger@gmail.com>
2018-05-28 18:03:52 +0200

    Added PropertyDAO

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/dao/PropertyDAO.java
@@ -0,0 +1,29 @@
+package com.openclassrooms.realestatemanager.database.dao;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+import android.database.Cursor;
+
+import com.openclassrooms.realestatemanager.managers.PropertiesMgr;
+import com.openclassrooms.realestatemanager.models.Property;
+
+import java.util.List;
+
+@Dao
+public interface PropertyDAO {
+
+    @Query("SELECT * FROM Property")
+    LiveData<List<Property>> getProperties();
+
+    @Query("SELECT * FROM Property WHERE id = :propertyId")
+    LiveData<Property> getProperty(long propertyId);
+
+    @Insert
+    long insertProperty(Property property);
+
+    @Update
+    int updateProperty(Property property);
+}
