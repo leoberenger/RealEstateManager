51350fa23ebee03244f5893a5e140e47ea27eef5
leoberenger <leoberenger@gmail.com>
2018-06-18 10:41:26 +0200

    Added MapView to DetailFragment

--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/fragments/DetailFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/fragments/DetailFragment.java
@@ -10,8 +10,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.openclassrooms.realestatemanager.R;
-import com.openclassrooms.realestatemanager.controllers.activities.MainActivity;
 import com.openclassrooms.realestatemanager.models.Property;
 
 import butterknife.BindView;
@@ -20,12 +26,18 @@ import butterknife.ButterKnife;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DetailFragment extends Fragment {
+public class DetailFragment extends Fragment implements OnMapReadyCallback {
 
     String TAG = "DetailFragment";
 
     //FOR DATA
     private Property property;
+
+    //FOR MAP
+    private LatLng propertyCoordinates;
+    private String propertyTitle;
+    private GoogleMap mMap;
+    MapView mapView;
 
     //FOR DESIGN
     @BindView(R.id.fragment_detail_description) TextView description;
@@ -45,10 +57,40 @@ public class DetailFragment extends Fragment {
 
         if(getArguments() != null) {
             property = getArguments().getParcelable(Property.PROPERTY_KEY);
+            propertyCoordinates = new LatLng(property.getLatitude(), property.getLongitude());
+            propertyTitle = property.getStreetNb() + " rue " + property.getStreetName() + ", " + property.getCity();
+            this.configureMap(view);
             updateShownProperty(property);
         }
 
         return view;
+    }
+
+
+    //-----------------------------------
+    // MAP CONFIGURATION
+    //-----------------------------------
+
+    private void configureMap(View v){
+        mapView = v.findViewById(R.id.map);
+        if (mapView != null) {
+            // Initialise the MapView
+            mapView.onCreate(null);
+            // Set the map ready callback to receive the GoogleMap object
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mMap = googleMap;
+        // Center camera on marker
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(propertyCoordinates, 20f));
+        mMap.addMarker(new MarkerOptions()
+                .position(propertyCoordinates)
+                .title(propertyTitle))
+                .showInfoWindow();
     }
 
     // -----------------
