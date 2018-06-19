0347854ed8eb48983b2829df00921d4a321289e7
leoberenger <leoberenger@gmail.com>
2018-06-19 10:04:56 +0200

    Added Get Photos from phone

--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/fragments/EditionFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/fragments/EditionFragment.java
@@ -1,21 +1,34 @@
 package com.openclassrooms.realestatemanager.controllers.fragments;
 
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.controllers.activities.EditionActivity;
 import com.openclassrooms.realestatemanager.models.Property;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,8 +39,20 @@ public class EditionFragment extends Fragment {
         // Required empty public constructor
     }
 
+
+    @BindView(R.id.edition_image_preview)
+    ImageView imageViewPreview;
+
     //FOR CALLBACK
     OnEditionListener mCallback;
+
+
+
+    //STATIC DATA FOR PICTURE
+    private static final String PERMS = Manifest.permission.READ_EXTERNAL_STORAGE;
+    private static final int RC_IMAGE_PERMS = 100;
+    private static final int RC_CHOOSE_PHOTO = 200;
+    private Uri uriImageSelected;
 
     public interface OnEditionListener{
         void onPropertyEdited(Property property);
@@ -101,6 +126,60 @@ public class EditionFragment extends Fragment {
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
                     + " must implement OnPropertiesListSelectedListener");
+        }
+    }
+
+    // --------------------------
+    // RETRIEVE PHOTO FROM PHONE
+    // --------------------------
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+
+    @OnClick(R.id.edition_add_photo_button)
+    // 3 - Ask permission when accessing to this listener
+    @AfterPermissionGranted(RC_IMAGE_PERMS)
+    public void onClickAddFile() {
+        this.chooseImageFromPhone();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 6 - Calling the appropriate method after activity result
+        this.handleResponse(requestCode, resultCode, data);
+    }
+
+    // --------------------
+    // FILE MANAGEMENT
+    // --------------------
+
+    private void chooseImageFromPhone(){
+        if (!EasyPermissions.hasPermissions(getContext(), PERMS)) {
+            EasyPermissions.requestPermissions(this, getString(R.string.popup_title_permission_files_access), RC_IMAGE_PERMS, PERMS);
+            return;
+        }
+        // 3 - Launch an "Selection Image" Activity
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, RC_CHOOSE_PHOTO);
+    }
+
+    // 4 - Handle activity response (after user has chosen or not a picture)
+    private void handleResponse(int requestCode, int resultCode, Intent data){
+        if (requestCode == RC_CHOOSE_PHOTO) {
+            if (resultCode == RESULT_OK) { //SUCCESS
+                this.uriImageSelected = data.getData();
+                Glide.with(this) //SHOWING PREVIEW OF IMAGE
+                        .load(this.uriImageSelected)
+                        .into(this.imageViewPreview);
+            } else {
+                Toast.makeText(getContext(), "No photo selected", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
